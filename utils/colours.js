b9c0671a2cc2c2c4f215861f666d3c98dbdf8a0b
jamie <j_blaut@hotmail.com>
2023-03-16 21:54:59 +0000

    add utils and allow js imports

---
+++ utils/colours.js
@@ -0,0 +1,8 @@
+export const white = '#fff';
+export const black = '#222';
+export const magenta = '#FF007C';
+export const coral = '#F8D8D6';
+export const turqoise = '#007C81';
+export const navy = '#081955';
+
+export default { white, black, magenta, coral, turqoise, navy };
---
+++ utils/fonts.js
@@ -0,0 +1,4 @@
+export const mainFont = '"Inconsolata", monospace';
+export const headingFont = '"Inter", sans-serif';
+
+export default { mainFont, headingFont };
